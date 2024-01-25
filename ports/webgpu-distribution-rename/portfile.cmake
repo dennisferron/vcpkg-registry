5b4849c4d7763abfb82538214a35a32b773670f0
Dennis Ferron <system.windows.codeslinger@gmail.com>
2024-01-24 21:34:49 -0600

    Since Windows is case insensitive renaming a folder to same name different case is hard. Commit 1 of 2.

--- ports/webgpu-distribution-rename/portfile.cmake
+++ ports/webgpu-distribution-rename/portfile.cmake
@@ -1,8 +1,11 @@
 vcpkg_from_github(
 	OUT_SOURCE_PATH SOURCE_PATH
 	REPO dennisferron/WebGPU-distribution
+	
 	REF fb39c1fcbd3c48da8e87fe614194a217a6e04fae
+	
 	SHA512 bf4bd2fef864e8120bfe65d11e9c370ab167da86e376c901eb4d45643f80008df0cdd09aad70fd0a1ed55cc962bf981eb46f76371feaad7ba2dc86c6fc78e646
+	
 	HEAD_REF main
 )
 
